50282a7644a9ff49540bafba6ba386af7aebff08
Tom Fuller <tomf95@hotmail.co.uk>
2016-11-02 19:05:28 +0000

    works up to 10

--- lib/roman_numeral.rb
+++ lib/roman_numeral.rb
@@ -1,3 +1,9 @@
 def roman_numeral(number)
-number == 1 ? 'I' : number == 2 ? 'II' : number == 3 ? 'III' : 'IV'
+  if number <= 5
+    number == 1 ? 'I' : number == 2 ? 'II' : number == 3 ? 'III' : number == 4 ? 'IV' : 'V'
+  elsif number > 5 && number < 10
+    number == 6 ? 'VI' : number == 7 ? 'VII' : number == 8 ? 'VIII' : 'IX'
+  elsif number == 10
+    'X'
+  end
 end
--- spec/roman_numeral_spec.rb
+++ spec/roman_numeral_spec.rb
@@ -20,3 +20,33 @@ describe 'roman_numeral' do
     expect(roman_numeral(4)).to eq 'IV'
   end
 end
+describe 'roman_numeral' do
+  it 'return "V" when 5 is passed' do
+    expect(roman_numeral(5)).to eq 'V'
+  end
+end
+describe 'roman_numeral' do
+  it 'return "VI" when 6 is passed' do
+    expect(roman_numeral(6)).to eq 'VI'
+  end
+end
+describe 'roman_numeral' do
+  it 'return "VII" when 7 is passed' do
+    expect(roman_numeral(7)).to eq 'VII'
+  end
+end
+describe 'roman_numeral' do
+  it 'return "VIII" when 8 is passed' do
+    expect(roman_numeral(8)).to eq 'VIII'
+  end
+end
+describe 'roman_numeral' do
+  it 'return "IX" when 9 is passed' do
+    expect(roman_numeral(9)).to eq 'IX'
+  end
+end
+describe 'roman_numeral' do 
+  it 'return "X" when 10 is passed' do
+    expect(roman_numeral(10)).to eq 'X'
+  end
+end
